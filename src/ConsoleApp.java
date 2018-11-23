b96d83833f4c54214fa3da7bd9d9206a59368487
Samuel <samuel.rufinatscha@gmail.com>
2018-11-23 15:42:03 +0100

    Changing to version 1.3.1.

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -15,7 +15,7 @@ public class ConsoleApp {
 			MAML.minWeightMagnitude = Integer.parseInt(args[4]);
 		}
 		
-		System.out.println("MAM Lite 1.3\n");
+		System.out.println("MAM Lite 1.3.1\n");
 		
 		Scanner in = new Scanner(System.in);
 		
